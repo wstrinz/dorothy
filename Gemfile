5c28642eb95ec2bc8868fc2a189d405752b3c0f5
Grant Speelman <grant.speelman@ubxd.com>
2013-08-21 16:58:48 +0200

    add minitest/reporters

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ source "https://rubygems.org"
 # Include everything needed to run rake, tests, features, etc.
 group :development do
   gem "minitest"
+  gem "minitest-reporters"
   gem "rdoc"
   gem "bundler"
   #gem "jeweler"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use!
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'ext'))
 
